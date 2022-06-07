39a600b0060e70f19d361483f2bcb0fb023a8b73
CruzAPI <guilhermecruz1574@gmail.com>
2022-06-06 22:07:32 -0300

    Initial commit

---
+++ server/common/src/main/java/br/com/acenetwork/common/Common.java
@@ -0,0 +1,8 @@
+package br.com.acenetwork.common;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Common extends JavaPlugin
+{
+
+}
---
+++ server/gamemode/craftlandia/src/main/java/br/com/acenetwork/craftlandia/Main.java
@@ -0,0 +1,108 @@
+package br.com.acenetwork.craftlandia;
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import br.com.acenetwork.common.Common;
+
+public class Main extends Common implements Listener
+{
+	@Override
+	public void onEnable()
+	{
+		super.onEnable();
+		
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler
+	public void a(InventoryOpenEvent e)
+	{
+		Inventory inv = e.getInventory();
+		
+		if(inv.getType() == InventoryType.ENCHANTING)
+		{
+			inv.setItem(1, new ItemStack(Material.INK_SACK, 3, (short) 4));
+		}
+	}
+	
+	@EventHandler
+	public void a(InventoryCloseEvent e)
+	{
+		Inventory inv = e.getInventory();
+		
+		if(inv.getType() == InventoryType.ENCHANTING)
+		{
+			inv.setItem(1, null);
+		}
+	}
+	
+	@EventHandler
+	public void a(InventoryDragEvent e)
+	{
+		if(e.getInventory().getType() == InventoryType.ENCHANTING)
+		{
+			if(e.getRawSlots().contains(1))
+			{
+				e.setCancelled(true);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void a(InventoryClickEvent e)
+	{
+		InventoryAction action = e.getAction();
+		int rawSlot = e.getRawSlot();
+		ItemStack current = e.getCurrentItem();
+		ItemStack cursor = e.getCursor();
+		
+		if(e.getInventory().getType() == InventoryType.ENCHANTING)
+		{
+			if(rawSlot == 1)
+			{
+				e.setCancelled(true);
+			}
+			else if(current != null && current.getType() == Material.INK_SACK && current.getDurability() == (short) 4
+					&& action == InventoryAction.MOVE_TO_OTHER_INVENTORY)
+			{
+				e.setCancelled(true);
+			}
+			else if(cursor != null && cursor.getType() == Material.INK_SACK && cursor.getDurability() == (short) 4
+					&& action == InventoryAction.COLLECT_TO_CURSOR)
+			{
+				e.setCancelled(true);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void a(EnchantItemEvent e)
+	{
+		Player p = e.getEnchanter();
+		Inventory inv = e.getInventory();
+		
+		Bukkit.getScheduler().runTask(this, () ->
+		{
+			if(p.getGameMode() != GameMode.CREATIVE)
+			{
+				p.setLevel(p.getLevel() - (e.getExpLevelCost() - (1 + e.whichButton())));
+				inv.setItem(1, new ItemStack(Material.INK_SACK, 3, (short) 4));
+			}
+		});
+	}
+}
