80877fba854804aba605ba4f71bb12dd748c960e
CruzAPI <guilhermecruz1574@gmail.com>
2022-06-27 22:50:38 -0300

    Add deposit

---
+++ server/common/src/main/java/br/com/acenetwork/commons/executor/Deposit.java
@@ -0,0 +1,147 @@
+package br.com.acenetwork.commons.executor;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import br.com.acenetwork.commons.Common;
+import br.com.acenetwork.commons.CommonsUtil;
+import br.com.acenetwork.commons.constants.Currency;
+import br.com.acenetwork.commons.event.SocketEvent;
+import br.com.acenetwork.commons.manager.CommonsConfig;
+import br.com.acenetwork.commons.manager.CommonsConfig.Type;
+import br.com.acenetwork.commons.manager.Message;
+import br.com.acenetwork.commons.player.CommonPlayer;
+import br.com.acenetwork.commons.player.craft.CraftCommonPlayer;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
+
+public class Deposit implements TabExecutor, Listener
+{
+	public Deposit()
+	{
+		Bukkit.getPluginManager().registerEvents(this, Common.getPlugin());
+	}
+	
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) 
+	{
+		return new ArrayList<>();
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String aliases, String[] args)
+	{
+		ResourceBundle bundle = ResourceBundle.getBundle("message");
+		
+		if(!(sender instanceof Player))
+		{
+			TextComponent text = new TextComponent(bundle.getString("commons.cmds.cant-perform-command"));
+			text.setColor(ChatColor.RED);
+			CommonsUtil.sendMessage(sender, text);
+			return true;
+		}
+
+		Player p = (Player) sender;
+		CommonPlayer cp = CraftCommonPlayer.get(p);
+		
+		bundle = ResourceBundle.getBundle("message", cp.getLocale());
+
+		if(args.length == 1)
+		{
+			try
+			{
+				int amount = Integer.valueOf(args[0]);
+				
+				if(cp.isRequesting())
+				{
+					return true;
+				}
+				
+				Runtime.getRuntime().exec(String.format("node %s/reset/deposit %s %s %s %s", System.getProperty("user.home"),
+						Common.getSocketPort(), 
+						cp.requestDatabase(), 
+						p.getUniqueId(), 
+						amount));
+			}
+			catch(NumberFormatException e)
+			{
+				p.sendMessage(ChatColor.RED + bundle.getString("commons.cmds.invalid-number-format"));
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+				p.sendMessage(ChatColor.RED + bundle.getString("commons.unexpected-error"));
+			}
+		}
+		else
+		{
+			TextComponent[] extra = new TextComponent[1];
+			
+			extra[0] = new TextComponent("/" + aliases + " <" + bundle.getString("commons.words.amount") + ">");
+			
+			TextComponent text = Message.getTextComponent(bundle.getString("commons.cmds.wrong-syntax-try"), extra);
+			text.setColor(ChatColor.RED);
+			CommonsUtil.sendMessage(sender, text);
+		}
+
+		return true;
+	}
+	
+	@EventHandler
+	public void onSocketEvent(SocketEvent e)
+	{
+		String[] args = e.getArgs();
+		
+		String cmd = args[0];
+		
+		if(cmd.equalsIgnoreCase("deposit"))
+		{
+			int taskId = Integer.valueOf(args[1]);
+			UUID uuid = UUID.fromString(args[2]);
+			Player p = Bukkit.getPlayer(uuid);
+			CommonPlayer cp = CraftCommonPlayer.get(p);
+			int amount = Integer.valueOf(args[3]);
+			
+			if(cp != null && Bukkit.getScheduler().isQueued(taskId))
+			{
+//				ResourceBundle bundle = ResourceBundle.getBundle("message", cp.getLocale());
+				Bukkit.getScheduler().cancelTask(taskId);
+			}
+			
+			File file = CommonsConfig.getFile(Type.PLAYER, true, uuid);
+			YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+			
+			DecimalFormat df = new DecimalFormat("#.##");
+			
+			double $bta = config.getDouble(Currency.$BTA.getYamlKey());
+			Bukkit.broadcastMessage(p.getName() + " oldBta: " + df.format($bta));
+			$bta += amount;
+			config.set(Currency.$BTA.getYamlKey(), $bta);
+			
+			try
+			{
+				config.save(file);
+			}
+			catch(IOException ex)
+			{
+				ex.printStackTrace();
+			}
+			
+			Bukkit.broadcastMessage(p.getName() + " newBta: " + df.format($bta));
+		}
+	}
+}
